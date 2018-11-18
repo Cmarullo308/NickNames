ded6e0ec99860d63e5248c8c5624710a5d95d741
BuildTools <unconfigured@null.spigotmc.org>
2018-11-17 19:28:57 -0500

    Added color codes

--- src/me/NickNames/main/Main.java
+++ src/me/NickNames/main/Main.java
@@ -9,20 +9,78 @@ import me.NickNames.main.MyEvents;
 import net.md_5.bungee.api.ChatColor;
 
 public class Main extends JavaPlugin {
+	// Messages
 	final String invalidNumOfArgsMessage = ChatColor.RED + "Invalid number of arguements";
 	final String noPermissionMessage = ChatColor.RED + "You do not have permission to run this command";
 	final String mustBeAPlayerMessage = ChatColor.RED + "Must be a player to run this command";
 	final String nickNameSet = ChatColor.GRAY + "Nickname set";
 	final String nickNameDisabled = ChatColor.GRAY + "Nickname disabled";
+	final String colorCodes = "--Chat Colors--\n" + ChatColor.DARK_RED + "&4 : " + "DARK RED\n" + ChatColor.RED
+			+ "&c : " + "RED\n" + ChatColor.GOLD + "&6 : " + "GOLD\n" + ChatColor.YELLOW + "&e : " + "YELLOW\n"
+			+ ChatColor.DARK_GREEN + "&2 : " + "DARK GREEN\n" + ChatColor.GREEN + "&a : " + "GREEN\n" + ChatColor.AQUA
+			+ "&b : " + "AQUA\n" + ChatColor.DARK_AQUA + "&3 : " + "DARK AQUA\n" + ChatColor.DARK_BLUE + "&1 : "
+			+ "DARK BLUE\n" + ChatColor.BLUE + "&9 : " + "BLUE\n" + ChatColor.LIGHT_PURPLE + "&d : " + "LIGHT PURPLE\n"
+			+ ChatColor.DARK_PURPLE + "&5 : " + "DARK PURPLE\n" + ChatColor.WHITE + "&f : " + "WHITE\n" + ChatColor.GRAY
+			+ "&7 : " + "GRAY\n" + ChatColor.DARK_GRAY + "&8 : " + "DARK GRAY\n" + ChatColor.BLACK + "&0 : " + "BLACK";
+	// ---------
 
 	public NickNameData nickNamesData;
 
 	@Override
 	public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+		String subCommand = args[0].toLowerCase();
 
-		nickCommand(sender, args);
+		if (subCommand.equals("help")) {
+			helpMenu(sender, args);
+		} else if (subCommand.equals("lookup")) {
+			lookup(sender, args);
+		} else if (subCommand.equals("colorcodes")) {
+			sender.sendMessage(colorCodes);
+		} else {
+			nickCommand(sender, args);
+		}
 
 		return true;
+
+	}
+
+	private void lookup(CommandSender sender, String[] args) {
+		if (!sender.hasPermission("nicknames.lookup")) {
+			sender.sendMessage(noPermissionMessage);
+			return;
+		}
+
+		boolean multiple = false;
+		String message = "";
+		Object[] keys = nickNamesData.getNickNames().getKeys(false).toArray();
+
+		for (int userId = 0; userId < keys.length; userId++) {
+			if (nickNamesData.getNickNames().getString(keys[userId] + ".nickname").equals(args[1])) {
+				if (!multiple) {
+					message += args[1] + " is " + nickNamesData.getNickNames().getString(keys[userId] + ".username");
+					multiple = true;
+				} else {
+					message += ", " + nickNamesData.getNickNames().getString(keys[userId] + ".username");
+				}
+			}
+		}
+
+		sender.sendMessage(message);
+	}
+
+	private void helpMenu(CommandSender sender, String[] args) {
+		String message = ChatColor.GRAY + "";
+
+		message += "Commands:\n";
+		message += "/nick <nickname>: Sets your own nickname\n";
+		message += "/nick <nickname> off: Removes your nickname\n";
+		message += "/nick <player name> <nickname>: Sets another players nickname\n";
+		message += "/nick <player name> off: Removes another players nickname\n";
+		message += "/nick lookup <nickname>: Looks up a username from a nickname\n";
+		message += "/nick colorcodes: Shows the color codes";
+		// finish
+
+		sender.sendMessage(message);
 	}
 
 	private void nickCommand(CommandSender sender, String[] args) {
@@ -34,6 +92,11 @@ public class Main extends JavaPlugin {
 		else if (args.length == 1) {
 			if (!(sender instanceof Player)) {
 				sender.sendMessage(mustBeAPlayerMessage);
+				return;
+			}
+
+			if (!sender.hasPermission("nicknames.set")) {
+				sender.sendMessage(noPermissionMessage);
 				return;
 			}
 
@@ -49,6 +112,11 @@ public class Main extends JavaPlugin {
 		}
 		// Other nickname
 		else if (args.length == 2) {
+			if (!sender.hasPermission("nicknames.set.other")) {
+				sender.sendMessage(noPermissionMessage);
+				return;
+			}
+
 			Player player = getServer().getPlayer(args[0]);
 			if (player == null) {
 				sender.sendMessage(ChatColor.RED + args[0] + " does not exist in this server");
@@ -81,22 +149,24 @@ public class Main extends JavaPlugin {
 	}
 
 	private String changeColorCodes(String name) {
-		name = name.replace("&4", ChatColor.DARK_RED.toString());
-		name = name.replace("&c", ChatColor.RED.toString());
-		name = name.replace("&6", ChatColor.GOLD.toString());
-		name = name.replace("&e", ChatColor.YELLOW.toString());
-		name = name.replace("&2", ChatColor.DARK_GREEN.toString());
-		name = name.replace("&a", ChatColor.GREEN.toString());
-		name = name.replace("&b", ChatColor.AQUA.toString());
-		name = name.replace("&3", ChatColor.DARK_AQUA.toString());
-		name = name.replace("&1", ChatColor.DARK_BLUE.toString());
-		name = name.replace("&9", ChatColor.BLUE.toString());
-		name = name.replace("&d", ChatColor.LIGHT_PURPLE.toString());
-		name = name.replace("&5", ChatColor.DARK_PURPLE.toString());
-		name = name.replace("&f", ChatColor.WHITE.toString());
-		name = name.replace("&7", ChatColor.GRAY.toString());
-		name = name.replace("&8", ChatColor.DARK_GRAY.toString());
-		name = name.replace("&0", ChatColor.BLACK.toString());
+		if (name != null) {
+			name = name.replace("&4", ChatColor.DARK_RED.toString());
+			name = name.replace("&c", ChatColor.RED.toString());
+			name = name.replace("&6", ChatColor.GOLD.toString());
+			name = name.replace("&e", ChatColor.YELLOW.toString());
+			name = name.replace("&2", ChatColor.DARK_GREEN.toString());
+			name = name.replace("&a", ChatColor.GREEN.toString());
+			name = name.replace("&b", ChatColor.AQUA.toString());
+			name = name.replace("&3", ChatColor.DARK_AQUA.toString());
+			name = name.replace("&1", ChatColor.DARK_BLUE.toString());
+			name = name.replace("&9", ChatColor.BLUE.toString());
+			name = name.replace("&d", ChatColor.LIGHT_PURPLE.toString());
+			name = name.replace("&5", ChatColor.DARK_PURPLE.toString());
+			name = name.replace("&f", ChatColor.WHITE.toString());
+			name = name.replace("&7", ChatColor.GRAY.toString());
+			name = name.replace("&8", ChatColor.DARK_GRAY.toString());
+			name = name.replace("&0", ChatColor.BLACK.toString());
+		}
 
 		return name;
 	}
--- src/me/NickNames/main/MyEvents.java
+++ src/me/NickNames/main/MyEvents.java
@@ -2,13 +2,34 @@ package me.NickNames.main;
 
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLoginEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+
+import net.md_5.bungee.api.ChatColor;
 
 public class MyEvents implements Listener {
 	Main main;
 
 	public MyEvents(Main main) {
 		this.main = main;
+	}
+
+	@EventHandler
+	public void onQuit(PlayerQuitEvent event) {
+		String nickName = main.nickNamesData.getNickNames().getString(event.getPlayer().getUniqueId() + ".nickname");
+
+		if (nickName != null) {
+			event.setQuitMessage(nickName + ChatColor.YELLOW + " has left the game");
+		}
+	}
+
+	@EventHandler
+	public void onJoin(PlayerJoinEvent event) {
+		String nickName = main.nickNamesData.getNickNames().getString(event.getPlayer().getUniqueId() + ".nickname");
+		if (nickName != null) {
+			event.setJoinMessage(nickName + ChatColor.YELLOW + " has joined the server");
+		}
 	}
 
 	@EventHandler
